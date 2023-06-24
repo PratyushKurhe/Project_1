078999a62ef95bd5f333ed87db7c36f56fad0fd1
Pratyush Kurhe <pratyush.kurhe@gmail.com>
2023-06-24 18:37:04 +0530

    first commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Navigation()
+{
+    return(
+        <nav className="container">
+      <div className="logo">
+        <img src="/images/nike img.png" alt="logo img"></img>
+      </div>
+      <ui>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ui>
+      <button>login</button>
+    </nav>
+    );
+
+}
+
+export default Navigation;
